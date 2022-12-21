bdbc36fc2a684d04e933e41889a8852cdef02a35
Dijoux <chezdijoux@gmail.com>
2022-12-21 23:17:45 +0100

    version2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 qui va contenir notre serveur ainsi que les méthodes dont nous aurons besoin pour le faire fonctionner.*/
 
 const client = require('./connection.js')
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
@@ -25,14 +25,82 @@ app.listen(3000, ()=>{
 
 client.connect();
 
-//Connexion 
+//Connexion à la BDD sur le localhost:3030 + requête pour afficher toute la table defibrilator
 
 app.get('/', (req, res)=>{
     client.query("SELECT * FROM defibrilator", (err, result)=>{
         if(!err){
             res.send(result.rows);
+            app.use(bodyParser.json());
         }
     });
+    client.end;
+})
+
+
+// Afficher la table en selectionnant par l'ID 
+
+app.get('/defibrilator/:id', (req, res)=>{
+    client.query(`Select * from defibrilator where id=${req.params.id}`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+})
+client.connect();
+
+
+
+// Ajouter de nouveau défibrilateur
+
+app.post('/defibrilator', (req, res)=> {
+    const user = req.body;
+    let insertQuery = `insert into defibrilator(serial, locationName, locationAdrr, state, electrodesExpiry) 
+                       values(${defibrilator.id}, '${defibrilator.serial}', '${defibrilator.locationName}', '${defibrilator.locationAdrr}', '${defibrilator.state}', '${defibrilator.electrodesExpiry}')`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Insertion was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+})
+
+
+// Mettre à jour les défibrilateurs
+
+app.put('/defibrilator/:id', (req, res)=> {
+    let defibrilator = req.body;
+    let updateQuery = `update defibrilator
+                       set serial= '${defibrilator.serial}',
+                       locationName = '${defibrilator.locationName}',
+                       locationadrr = '${defibrilator.locationAdrr}',
+                       state = '${defibrilator.state}',
+                       electrodesExpiry = '${defibrilator.electrodesExpiry}',
+                       where id = ${defibrilator.id}`
+
+    client.query(updateQuery, (err, result)=>{
+        if(!err){
+            res.send('Update was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+})
+
+//Supprimer donnée de la table défibrilateur 
+
+app.delete('/defibrilator/:id', (req, res)=> {
+    let insertQuery = `DELETE FROM defibrilator WHERE id=${req.params.id}`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Deletion was successful')
+        }
+        else{ console.log(err.message) }
+    })
     client.end;
 })
 
